23a2e4ee038371b16e9554d8d478780b05ea337d
|°_°| <grivel.theo@protonmail.com>
2022-12-23 02:33:25 +0100

    main: create test_sum

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,7 +1,51 @@
 #include "Add.hpp"
 #include "Number.hpp"
 #include "color.hpp"
+#include <cctype>
 #include <iostream>
+#include <stdlib.h>
+
+static TreeNode *
+create_token(char **str)
+{
+	int number;
+
+	while (isspace(**str))
+		++(*str);
+
+	if (isdigit(**str))
+	{
+		number = atoi(*str);
+		while (isdigit(**str))
+			++(*str);
+		return new Number(number);
+	}
+	if (**str == '+')
+	{
+		++(*str);
+		return new Add();
+	}
+	return 0;
+}
+
+static void
+test_sum(char *str)
+{
+	TreeNode *number;
+	TreeNode *sum;
+
+	number = create_token(&str);
+	sum = create_token(&str);
+
+	sum->set_left(number);
+	number = create_token(&str);
+
+	sum->set_right(number);
+
+	sum->print();
+
+	delete sum;
+}
 
 static void
 test(void)
@@ -20,6 +64,7 @@ main(void)
 	title("Parsmath");
 
 	test();
+	test_sum((char *)"1 + 2");
 
 	return 0;
 }
